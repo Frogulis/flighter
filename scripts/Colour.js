0c406ecde2e290c2fe7fb1f836d0603b4feac9fb
Jamie Hoffmann <jamsarhoff@hotmail.com>
2018-02-25 19:23:39 +1100

    added HSL to Colour
    - a Colour can now be set with HSL
    - no HSL constructor though

--- scripts/Colour.js
+++ scripts/Colour.js
@@ -10,6 +10,13 @@ function Colour(r, g, b) {
 }
 
 Colour.prototype = {
+    setHSL: function(h, s, l)
+    {
+        var colours = this._hslToRgb(h, s, l);
+        this.r = colours[0];
+        this.g = colours[1];
+        this.b = colours[2];
+    },
     getHex: function()
     {
         function toTwo(str)
@@ -25,6 +32,30 @@ Colour.prototype = {
         return "#" + toTwo(this.r.toString(16)) +
                      toTwo(this.g.toString(16)) +
                      toTwo(this.b.toString(16));
+    },
+    _hslToRgb: function(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
     }
 };
 
